3a03ce7d8b09dd1e486018da4206f526b645eaa3
Andre Morata <andre.morata@projectmanager.com.br>
2017-07-20 22:11:27 -0300

    initial commit

---
+++ ModernStore.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModernStore.Domain.Entities;
+
+namespace ModernStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("andremorata", "andremorata");
+            var Customer = new Customer(
+                "Andre",
+                "Morata",
+                "andremorata@gmail.com",
+                user
+            );
+
+            Customer.Update("Andre Luis", "Morata Fernandes", new DateTime(1983, 9, 13));
+            
+            if (Customer.Notifications.Count > 0)
+                foreach (var item in Customer.Notifications)
+                    Console.WriteLine(item.Message);
+
+            Console.WriteLine(Customer.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ModernStore.Domain/Entities/Customer.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidator;
+using ModernStore.Shared.Entities;
+using System;
+
+namespace ModernStore.Domain.Entities
+{
+    public class Customer : Entity
+    {
+        public Customer (string firstName, string lastName, string email, User user)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = null;
+            Email = email;
+            User = user;
+
+            //validações
+            new ValidationContract<Customer>(this)
+                .IsRequired(x => x.FirstName, "Nome é obrigatório")
+                .HasMaxLenght(x => x.FirstName, 60, "Nome precisa ter 60 caracteres no máximo")
+                .HasMinLenght(x => x.LastName, 3, "Nome precisa ter ao menos 3 caracteres")
+                .IsRequired(x => x.LastName, "Sobrenome é obrigatório")
+                .HasMaxLenght(x => x.LastName, 60, "Sobrenome precisa ter 60 caracteres no máximo")
+                .HasMinLenght(x => x.LastName, 3, "Sobrenome precisa ter ao menos 3 caracteres")
+                .IsRequired(x => x.Email, "Email é obrigatório")
+                .HasMaxLenght(x => x.Email, 60, "Email precisa ter 60 caracteres no máximo")
+                .HasMinLenght(x => x.Email, 3, "Email precisa ter ao menos 3 caracteres")
+                .IsEmail(x => x.Email, "Email inválido");
+
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime? BirthDate { get; private set; }
+        public string Email { get; private set; }
+        public User User { get; private set; }
+
+        public void Update(string firstName, string lastName, DateTime? birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+    }
+}
